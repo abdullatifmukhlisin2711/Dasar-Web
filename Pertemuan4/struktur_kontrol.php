998f395551d5e2cb8ee0423164ae363af792902e
abdullatifmukhlisin2711 <abdul.latifm2711@gmail.com>
2024-09-20 13:45:02 +0700

    pertemuan 4

---
+++ Pertemuan4/struktur_kontrol.php
@@ -0,0 +1,23 @@
+<?php
+$nilaiNumerik = 92;
+
+if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100){
+    echo "Nilai huruf A: ";
+}elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90 ){
+    echo "Nilai huruf B= ";
+}elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80){
+    echo "Nilai huruf C= ";
+}elseif ($nilaiNumerik < 70){
+    echo "Nilai huruf D: ";
+}
+
+$jarakSaatini = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatini < $jarakTarget){
+    $jarakSaatini += $peningkatanHarian;
+    $hari++;
+}
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
