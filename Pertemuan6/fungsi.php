87f7fd26c74f0712a5cbfc19d278c9004a04ebac
abdullatifmukhlisin2711 <abdul.latifm2711@gmail.com>
2024-10-04 08:19:28 +0700

    Pertemuan 6

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -1,8 +1,15 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Latif <br/>";
-    echo "Senang berkenalan dengan anda<br/>";
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
 }
-perkenalan();
+perkenalan("Hamdana","Hallo");
+
+echo "<hr>";
+
+$saya = "Latif";
+$ucapanSalam = "Selamat Pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
