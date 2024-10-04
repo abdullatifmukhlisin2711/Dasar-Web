492b8189a0a7dbff558408459133075e652a67e0
abdullatifmukhlisin2711 <abdul.latifm2711@gmail.com>
2024-10-04 08:29:34 +0700

    Pertrmuan 6

--- Pertemuan6/fungsi2.php
+++ Pertemuan6/fungsi2.php
@@ -3,6 +3,12 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
-echo "Umur saya adalah ". hitungUmur(2004, 2024). "tahun";
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
+    echo "Saya berusia ".hitungUmur(2004, 2024)." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+perkenalan ("Latif")
 ?>
