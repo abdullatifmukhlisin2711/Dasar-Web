3a33da158c8ea1a2229dcfb08b8b450b900b900b
abdullatifmukhlisin2711 <abdul.latifm2711@gmail.com>
2024-10-11 00:12:07 +0700

    Pertemuan 7

---
+++ Pertemuan7/form_ajax.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contohh Form dengan PHP JQuery</title>
+        <script src="htt"
---
+++ Pertemuan7/html_aman.php
@@ -0,0 +1,7 @@
+<?php
+$input = $_POST['input'];
+$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+
+<script></script>
+
+?>
