dc6872ee2e767fc82e2bf161cf58e71ca066de01
abdullatifmukhlisin2711 <abdul.latifm2711@gmail.com>
2024-10-11 13:19:07 +0700

    Pertemuan 7

--- Pertemuan7/form_ajax.php
+++ Pertemuan7/form_ajax.php
@@ -2,4 +2,56 @@
 <html>
     <head>
         <title>Contohh Form dengan PHP JQuery</title>
-        <script src="htt"
+        <script src="jquery-3.7.1js"></script>
+</head>
+<body>
+    <h2>Form Contoh</h2>
+    <form id="myForm">
+    <label for="buah">Pilih Buah:</label>
+        <select name="buah" id="buah">
+            <option value="apel">Apel</option>
+            <option value="pisang">Pisang</option>
+            <option value="mangga">Mangga</option>
+            <option value="jeruk">Jeruk</option>
+</select>
+
+<br>
+
+<label>Pilih Warna Favorit:</label><br>
+<input type="checkbox" name="warna[]" value="merah">Merah<br>
+<input type="checkbox" name="warna[]" value="biru">Biru<br>
+<input type="checkbox" name="warna[]" value="hijau">Hijau<br>
+
+<br>
+
+<label>Pilih Jenis Kelamin:</label><br>
+<input type="radio" name="jenis_kelamin" value="laki-laki">Laki-Laki<br>
+<input type="radio" name="jenis_kelamin" value="perempuan">Perempuan<br>
+
+<br>
+
+<input type="submit" value="Submit">
+</form>
+
+<div id="hasil">
+</div>
+
+<script>
+    $(document).ready(function(){
+        $("#myForm").submit(function(e){
+
+            var formData = $("myForm".serialize();
+
+            $.ajax({
+                url: "proses_lanjut.php",
+                type:"POST",
+                data: formData,
+                succes: function(response){
+                    $(#hasil).html(response);
+                }
+            });
+        });
+    });
+    </script>
+    </body>
+    </html>
